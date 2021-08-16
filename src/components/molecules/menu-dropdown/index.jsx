7c9be493e8e8da4d63da4cd09ecf1af089bc6b6f
NicolasOmar <nicolas.passerino@gmail.com>
2021-08-16 01:03:52 -0300

    Menu Dropdown Storybook added

--- src/components/molecules/menu-dropdown/index.jsx
+++ src/components/molecules/menu-dropdown/index.jsx
@@ -3,14 +3,16 @@ import { arrayOf, shape, string, func } from 'prop-types'
 // ELEMENTS
 import MenuItem from '../../elements/menu-item'
 
+const getTestId = index => (index ? `menu-option-${index}` : 'menu-dropdown')
+
 const MenuDropdown = ({ menuLabel, options }) => (
-  <div className="ui simple dropdown item">
+  <div className="ui simple dropdown item" data-testid={getTestId()}>
     {menuLabel}
     <i className="dropdown icon"></i>
 
     <div className="menu">
       {options.map((option, i) => (
-        <MenuItem key={`option-${i}`} {...option} />
+        <MenuItem data-testid={`${getTestId(i)}`} key={`${getTestId(i)}`} {...option} />
       ))}
     </div>
   </div>
@@ -25,5 +27,5 @@ MenuDropdown.propTypes = {
       label: string.isRequired,
       onClick: func
     })
-  )
+  ).isRequired
 }
