d9e826cf3f39ff1068405ada44c0cdc69d991ea2
NicolasOmar <nicolas.passerino@gmail.com>
2021-12-04 03:42:48 -0300

    Missing Label component added

---
+++ src/components/atoms/Label/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { string, bool } from 'prop-types'
+
+const Label = ({ labelText, isRequired = false }) => (
+  <label className="label">
+    {labelText}
+    {isRequired && <label style={{ color: 'red', marginLeft: '2px' }}>*</label>}
+  </label>
+)
+
+export default Label
+
+Label.propTypes = {
+  labelText: string.isRequired,
+  isRequired: bool
+}
