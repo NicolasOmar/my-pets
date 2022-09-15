7ce540b5f855f69e7e403cbf31890342ec63f805
NicolasOmar <nicolas.passerino@gmail.com>
2022-09-15 13:28:10 -0300

    Fixing Storybook build errors

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,11 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
     "@storybook/preset-create-react-app"
   ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
 }
