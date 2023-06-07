38903ef901e510727f27fe31cf5fae6146597923
NicolasOmar <nicolas.passerino@gmail.com>
2023-06-07 11:32:07 -0300

    Changing env secrets usage according vite. Adding missing redux dependencies

--- vite/src/main.jsx
+++ vite/src/main.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+// REDUCERS
+import userReducer from './redux/reducers'
+// APOLLO CLIENT
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+// COMPONENTS
+import App from  './components/app/app'
+// FUNCTIONS
+import { getLoggedUser } from './functions/local-storage'
+
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_APOLLO_URL
+})
+
+const authLink = setContext((_, { headers }) => {
+  const loggedUser = getLoggedUser()
+  const token = loggedUser && loggedUser.token
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const apolloClient = new ApolloClient({
+  uri: import.meta.env.VITE_APOLLO_URL,
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({ addTypename: false })
+})
+
+const store = configureStore({
+  reducer: {
+    userReducer
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
+    </Provider>
   </React.StrictMode>,
 )
