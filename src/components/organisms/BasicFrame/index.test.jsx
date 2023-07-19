95e70f5e869c35a1a058bed6e8ddf91492477ec6
NicolasOmar <nicolas.passerino@gmail.com>
2023-07-19 11:09:47 -0300

    Changes made on BasicFrame component

--- src/components/organisms/BasicFrame/index.test.jsx
+++ src/components/organisms/BasicFrame/index.test.jsx
@@ -4,9 +4,10 @@ import { render, screen } from '@testing-library/react'
 // COMPONENTS
 import BasicFrame from '.'
 // MOCKS
-import { withHeader, headerTestIds, titleTestIds } from './index.mocks.json'
+import { testing } from './index.mocks.json'
 
 describe('[BasicFrame]', () => {
+  const { withHeader, headerTestIds, titleTestIds } = testing
   const testIds = [...headerTestIds, ...titleTestIds]
 
   test('Should render the component with required props only', () => {
