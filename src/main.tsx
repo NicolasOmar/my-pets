4b0471b682610eeb7735f70a16a035d3d6b66e4a
NicolasOmar <nicolas.passerino@gmail.com>
2025-12-28 17:45:50 +0100

    Dependencies updated. Styled obtained from ui library directly

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppWrapper from './components/AppWrapper'
+import '../node_modules/reactive-bulma/dist/assets/reactive-bulma.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
